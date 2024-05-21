e95ab8c99ae520022d95c91c7a27caf6cfe81f63
Brandon-Jeremy <brandonnader1@gmail.com>
2024-05-21 16:15:54 +0300

    Added Move file for different move types

---
+++ Move.py
@@ -0,0 +1,19 @@
+import numpy
+from Piece import Piece
+from enum import Enum
+
+class MoveType(Enum):
+    NORMAL = 1
+    DOUBLE_PAWN_PUSH = 2
+    CASTLE = 3
+    EN_PASSANT = 4
+    PROMOTION = 5
+    CAPTURE = 6
+
+class Move:
+    def __init__(self, piece: Piece, color: str, start_tile: int, capture_tile: int, move_type: MoveType) -> None:
+        self.piece = piece
+        self.color = color
+        self.start_tile = start_tile
+        self.capture_tile = capture_tile
+        self.type = move_type
