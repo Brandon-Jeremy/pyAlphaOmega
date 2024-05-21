45db41ed5820b28bc6174896f7413dba3675a561
Brandon-Jeremy <brandonnader1@gmail.com>
2024-05-21 16:15:34 +0300

    Updated Piece file

--- Piece.py
+++ Piece.py
@@ -22,4 +22,13 @@ class Piece:
             'pawn': 1, 'rook': 5, 'knight': 3,
             'bishop': 3, 'queen': 9, 'king': 999
         }
-        return piece_values.get(type, 0)
+        negate = -1 if self.color == 'black' else 1
+        return piece_values.get(type, 0) * negate
+    
+    def get_piece_from_symbol(symbol:str) -> 'Piece':
+        piece_types = {
+            'P': 'pawn', 'R': 'rook', 'N': 'knight',
+            'B': 'bishop', 'Q': 'queen', 'K': 'king'
+        }
+        color = 'black' if symbol.islower() else 'white'
+        return Piece(piece_types[symbol.upper()], color)
